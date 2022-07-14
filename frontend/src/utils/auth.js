5752323034df772326ec30f80208cf68c46f0616
Zakhar Menshikov <zm681986@mail.ru>
2022-07-14 19:23:24 +0500

    Проекная №15

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "'https://api.mesto-full.nomoredomains.xyz'";
+const BASE_URL = "https://api.mesto-full.nomoredomains.xyz";
 
 function handleCheckResponse(res) {
     if (res.ok) {
