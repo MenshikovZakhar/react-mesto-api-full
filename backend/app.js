f8f529573775bcdedd771ed95832bc83584079d5
Zakhar Menshikov <zm681986@mail.ru>
2022-07-14 19:09:56 +0500

    Проекная №15

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { login, createUser } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 app.use(cors);
 mongoose.connect('mongodb://localhost:27017/mestodb');
